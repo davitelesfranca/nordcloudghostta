57423ab413a481324a4815b140a8b23f77bdb3c9
davitelesfranca <davi.teles.franca@gmail.com>
2022-02-10 18:54:31 -0300

    NORD 15 - correcting on S3 object path

--- s3.tf
+++ s3.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "observability_bucket_ghost" {
 resource "aws_s3_bucket_object" "script_file" {
   bucket = aws_s3_bucket.observability_bucket_ghost.id
   key    = "nordcloud_ghost_init.sh"
-  source = "/home/dfranca-dev/aws-ghost-deployment-main/terraform/user_data/nordcloud_ghost_init.sh"
+  source = "./user_data/nordcloud_ghost_init.sh"
 
   etag = filemd5("/home/dfranca-dev/aws-ghost-deployment-main/terraform/user_data/nordcloud_ghost_init.sh")
 }
